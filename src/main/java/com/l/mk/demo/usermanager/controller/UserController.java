943315dc1ae9df728af5c74ad2b356d0f5a823e1
mk-win7 <woaishisi@126.com>
2014-06-19 14:34:03 +0800

    能够用JSP解析EL和request.getAttribute("")来获得model里的数据

--- src/main/java/com/l/mk/demo/usermanager/controller/UserController.java
+++ src/main/java/com/l/mk/demo/usermanager/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.l.mk.demo.usermanager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
 	
 	@RequestMapping("/login")
-	public String login(Model model) {
-		
+	public String login(@RequestParam String username,
+						@RequestParam String password,
+						Model model) {
+		System.out.println(username + "：" + password);
+		model.addAttribute("name", username);
 		return "/show";
 	}
 	
--- src/main/webapp/WEB-INF/views/show.jsp
+++ src/main/webapp/WEB-INF/views/show.jsp
@@ -2,6 +2,7 @@
     pageEncoding="utf-8"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ page isELIgnored="false"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -11,7 +12,8 @@
 <body>
     <h2>欢迎来到用户中心</h2>
     
-    <p>用户名： </p>
+    <p>用户名：<%= request.getAttribute("name") %> </p>
+    <h3>${name}</h3>
     
 </body>
 </html>
