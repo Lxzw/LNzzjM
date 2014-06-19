ab969e2a8e9d446dcfbeb1707b2baf77147702ff
mk-win7 <woaishisi@126.com>
2014-06-19 16:22:00 +0800

    简单的实现了对用户名判断和认证

--- src/main/java/com/l/mk/demo/usermanager/controller/UserController.java
+++ src/main/java/com/l/mk/demo/usermanager/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.l.mk.demo.usermanager.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,13 +11,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UserController {
 	
+	private static Map<String,String> map = null;
+	
+	static {
+		map = new HashMap<String,String>();
+		map.put("admin","123456");
+		map.put("管理员","123" );
+	}
+	
 	@RequestMapping("/login")
 	public String login(@RequestParam String username,
 						@RequestParam String password,
 						Model model) {
-		System.out.println(username + "：" + password);
-		model.addAttribute("name", username);
-		return "/show";
+		if (map.get(username).equals(password)) {
+		    model.addAttribute("name", username);
+		    return "/show";
+		}
+		
+		model.addAttribute("error", "用户名不存在");
+		return "/index";
+		
 	}
 	
 	@RequestMapping({"/","/index"})
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,10 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=utf-8"
     pageEncoding="utf-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ page isELIgnored="false"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <body>
 <h2>用户登录</h2>
-
+<p>${error}</p>
 <form action="/LNzzjM/login" method="post">
   <p> 用户名： </p> <input type="text" name="username" />
   <p> 密   码：  </p> <input type="password" name="password" />
